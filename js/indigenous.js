b0f767887f2e79d8ba51ea4010f30d463b3091fc
swentel <swentel@realize.be>
2020-04-04 17:13:29 +0200

    Issue #21 - part 2: select in box

--- js/indigenous.js
+++ js/indigenous.js
@@ -70,6 +70,7 @@ function getTags(reload) {
             debug(data);
             if (data.categories) {
                 configSave('categories', data.categories);
+                setAutocomplete(categories);
             }
 
             if (reload) {
@@ -81,6 +82,58 @@ function getTags(reload) {
                 snackbar('Something went wrong with getting the tags', 'error');
             }
         });
+    }
+    else {
+        setAutocomplete(categories);
+    }
+}
+
+/**
+ * Set autocomplete.
+ *
+ * @param categories
+ */
+function setAutocomplete(categories) {
+    if (categories.length > 0) {
+
+        function split( val ) {
+            return val.split( /,\s*/ );
+        }
+        function extractLast( term ) {
+            return split( term ).pop();
+        }
+
+        $( "#tags" )
+            // don't navigate away from the field on tab when selecting an item
+            .on( "keydown", function( event ) {
+                if ( event.keyCode === $.ui.keyCode.TAB &&
+                    $( this ).autocomplete( "instance" ).menu.active ) {
+                    event.preventDefault();
+                }
+            })
+            .autocomplete({
+                minLength: 0,
+                source: function( request, response ) {
+                    // delegate back to autocomplete, but extract the last term
+                    response( $.ui.autocomplete.filter(
+                        categories, extractLast( request.term ) ) );
+                },
+                focus: function() {
+                    // prevent value inserted on focus
+                    return false;
+                },
+                select: function( event, ui ) {
+                    var terms = split( this.value );
+                    // remove the current input
+                    terms.pop();
+                    // add the selected item
+                    terms.push( ui.item.value );
+                    // add placeholder to get the comma-and-space at the end
+                    terms.push( "" );
+                    this.value = terms.join( ", " );
+                    return false;
+                }
+            });
     }
 }
 
--- preload.js
+++ preload.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     if (!!window && !(!!window.$)) {
         window.$ = window.jQuery = require('jquery');
         require ('tooltipster');
-        require('./js/indigenous.js')
+        require('./js/indigenous.js');
+        require('./js/jquery-ui.min.js')
     }
 });
